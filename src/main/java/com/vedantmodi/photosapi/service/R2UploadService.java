817bfd464c53fc79c5d90dbc317da604680f5068
Vedant Modi <vedantmodi@gmail.com>
2026-03-09 17:44:01 -0400

    add upload service

---
+++ src/main/java/com/vedantmodi/photosapi/service/R2UploadService.java
@@ -0,0 +1,35 @@
+package com.vedantmodi.photosapi.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+public class R2UploadService {
+
+    private final S3Client s3Client;
+
+    @Value("${r2.bucket}")
+    private String bucket;
+
+    @Value("${r2.public-url}")
+    private String publicUrl;
+
+    public R2UploadService(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String upload(byte[] data, String key, String contentType) {
+        s3Client.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(key)
+                        .contentType(contentType)
+                        .build(),
+                RequestBody.fromBytes(data)
+        );
+        return publicUrl + "/" + key;
+    }
+}
